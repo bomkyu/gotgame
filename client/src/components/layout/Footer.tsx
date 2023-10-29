846f1105154b9a9c203baf913c4170286517ddfd
bomkyu <a01090762806@gmail.com>
2023-10-29 16:23:29 +0900

    _ADD 반응형 hotizental_4 및 2, 푸터 추가

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 const footer = () => {
   return (
     <footer>
-      
+      <div className='inner'>
+        <div className='footer-box'>
+          <p className='footer-title'>GOATGAME</p>
+          <p className='footer-copy'>Copyright GoatGame. All rights reserved</p>
+        </div>
+      </div>
     </footer>
   )
 }

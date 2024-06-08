aa7364de4ce1fc9bec6f7919651febac6634b479
bomkyu <a01090762806@gmail.com>
2024-06-09 02:05:54 +0900

    fix. mysql 쿼리 없을때 종료되는부분을 cron 스케쥴로 5분마다 select해서 유지

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,13 @@
 // server/index.js 또는 서버 애플리케이션 진입 파일
-import * as cron from 'node-cron';
-import mysql from 'mysql2';
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { stat } from 'fs';
-import path from 'path';
+import * as cron from "node-cron";
+import mysql from "mysql2";
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import { stat } from "fs";
+import path from "path";
 dotenv.config(); // .env 파일의 환경 변수를 process.env에 등록
-
 
 const app = express();
 const port = process.env.SERVER_PORT; // .env 파일에서 등록한 환경 변수 사용
@@ -19,6 +18,10 @@ const connection = mysql.createConnection({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 28800000, // 8시간
 });
 
 // 미들웨어 등록
@@ -27,17 +30,17 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //카카오 토큰
-app.post('/api/token', (req : Request, res: Response) =>{
+app.post("/api/token", (req: Request, res: Response) => {
   //console.log(req.body)
-})
+});
 
 // 라우터 등록
-app.get('/api/data', (req: Request, res: Response) => {
-  const query = 'SELECT * FROM tb_user';
+app.get("/api/data", (req: Request, res: Response) => {
+  const query = "SELECT * FROM tb_user";
   connection.query(query, (err, results) => {
     if (err) {
-      console.error('쿼리부분 에러', err);
-      res.status(500).json({ error: 'An error occurred while fetching data.' });
+      console.error("쿼리부분 에러", err);
+      res.status(500).json({ error: "An error occurred while fetching data." });
     } else {
       res.json(results);
     }
@@ -45,10 +48,9 @@ app.get('/api/data', (req: Request, res: Response) => {
 });
 
 //매일 자정에 실행되는 스케줄링 작업
-cron.schedule('0 0 * * *', async () => {
+cron.schedule("0 0 * * *", async () => {
   //현재날짜
   const currentDate = new Date();
-
 
   const updateQuery = `
     UPDATE tb_posts
@@ -58,53 +60,77 @@ cron.schedule('0 0 * * *', async () => {
 
   connection.query(updateQuery, [currentDate], (err, results) => {
     if (err) {
-      console.error('Error updating status:', err);
+      console.error("Error updating status:", err);
     } else {
-      console.log('Status updated successfully');
+      console.log("Status updated successfully");
+    }
+  });
+});
+
+// 8시간마다 실행되는 스케줄링 작업
+cron.schedule("0 */8 * * *", async () => {
+  connection.query("SELECT 1", (err, results) => {
+    if (err) {
+      console.error("Error executing keep-alive query:", err);
+    } else {
+      console.log("Keep-alive query executed successfully");
+    }
+  });
+});
+
+// 5분마다 keep-alive 쿼리 실행
+cron.schedule("*/5 * * * *", async () => {
+  connection.query("SELECT 1", (err, results) => {
+    if (err) {
+      console.error("Error executing keep-alive query:", err);
+    } else {
+      console.log("Keep-alive query executed successfully");
     }
   });
 });
 
 //회원가입시, 중복되는 닉네임 검사를 위한 닉네임 가져오기
-app.get('/api/register', async (req: Request, res:Response) => {
+app.get("/api/register", async (req: Request, res: Response) => {
   const nickName = req.query.nickName;
-  const query = 'SELECT nickName FROM tb_user WHERE nickName = ?;'
+  const query = "SELECT nickName FROM tb_user WHERE nickName = ?;";
 
-  connection.query(query,[nickName], (err, results) => {
+  connection.query(query, [nickName], (err, results) => {
     if (err) {
-      console.error('쿼리부분 에러', err);
-      res.status(500).json({ error: 'An error occurred while fetching data.' });
+      console.error("쿼리부분 에러", err);
+      res.status(500).json({ error: "An error occurred while fetching data." });
     } else {
       res.json(results);
     }
-  })
-})
+  });
+});
 
-// 인증정보 유효성 검사 
-app.post('/api/register', async (req: Request, res: Response) => {
-  const query : string = `SELECT id, nickName, platform FROM tb_user WHERE id = '${req.body.id}'`
-  connection.query(query, (err, results : mysql.OkPacket[]) => {
+// 인증정보 유효성 검사
+app.post("/api/register", async (req: Request, res: Response) => {
+  const query: string = `SELECT id, nickName, platform FROM tb_user WHERE id = '${req.body.id}'`;
+  connection.query(query, (err, results: mysql.OkPacket[]) => {
     if (err) {
-      console.error('쿼리부분 에러', err);
-      res.status(500).json({ error: 'An error occurred while fetching data.' });
+      console.error("쿼리부분 에러", err);
+      res.status(500).json({ error: "An error occurred while fetching data." });
     } else {
       //console.log(results.length);
-      if(results.length > 0){
-        res.json({ status: 'login', userInfo : results[0] }); // 클라이언트에게 에러 응답 보내기
-      }else{
-        const {id, nickName, platform} = req.body
-        if(nickName === undefined){
-          res.json({status : 'register'})
-          return
-        }else{
-          const insertQuery = `INSERT INTO tb_user (id, nickName, platform) VALUES ('${id}','${nickName}', '${platform}')`
+      if (results.length > 0) {
+        res.json({ status: "login", userInfo: results[0] }); // 클라이언트에게 에러 응답 보내기
+      } else {
+        const { id, nickName, platform } = req.body;
+        if (nickName === undefined) {
+          res.json({ status: "register" });
+          return;
+        } else {
+          const insertQuery = `INSERT INTO tb_user (id, nickName, platform) VALUES ('${id}','${nickName}', '${platform}')`;
           connection.query(insertQuery, (err, insertResults) => {
             if (err) {
               //console.error('회원가입 쿼리 실행 중 에러', err);
-              res.status(500).json({ error: 'An error occurred while registering user.' });
+              res
+                .status(500)
+                .json({ error: "An error occurred while registering user." });
             } else {
               //console.log('회원가입 성공', insertResults);
-              res.json({ status: 'registerSuccess'});
+              res.json({ status: "registerSuccess" });
             }
           });
         }
@@ -113,86 +139,119 @@ app.post('/api/register', async (req: Request, res: Response) => {
   });
 });
 
-app.get('/api/main', async(req: Request, res: Response)=> {
-  const query = 'SELECT * FROM tb_posts ORDER BY date DESC'; 
+app.get("/api/main", async (req: Request, res: Response) => {
+  const query = "SELECT * FROM tb_posts ORDER BY date DESC";
   connection.query(query, (err, results) => {
     if (err) {
-      console.error('쿼리부분 에러', err);
-      res.status(500).json({ error: 'An error occurred while fetching data.' });
+      console.error("쿼리부분 에러", err);
+      res.status(500).json({ error: "An error occurred while fetching data." });
     } else {
       res.json(results);
     }
   });
 });
 
-app.get('/view/:num',async (req:Request, res:Response) => {
+app.get("/view/:num", async (req: Request, res: Response) => {
   const num = req.params.num;
-  const selectQuerry = `SELECT * FROM tb_posts WHERE num = ${num}`
+  const selectQuerry = `SELECT * FROM tb_posts WHERE num = ${num}`;
 
   connection.query(selectQuerry, (err, results) => {
     if (err) {
-      console.error('쿼리부분 에러', err);
-      res.status(500).json({ error: 'An error occurred while fetching data.' });
+      console.error("쿼리부분 에러", err);
+      res.status(500).json({ error: "An error occurred while fetching data." });
     } else {
       res.json(results);
     }
   });
-})
+});
 
-app.delete('/delete/:num', (req:Request, res:Response)=>{
+app.delete("/delete/:num", (req: Request, res: Response) => {
   const num = req.params.num;
-  const deleteQuery = `DELETE FROM tb_posts WHERE num = ?`
+  const deleteQuery = `DELETE FROM tb_posts WHERE num = ?`;
 
-  connection.query(deleteQuery, [num], (err, result : mysql.OkPacket ) => {
+  connection.query(deleteQuery, [num], (err, result: mysql.OkPacket) => {
     if (err) {
-      console.error('쿼리 부분 에러', err);
-      res.status(500).json({ error: '데이터 삭제 중 오류가 발생했습니다.' });
+      console.error("쿼리 부분 에러", err);
+      res.status(500).json({ error: "데이터 삭제 중 오류가 발생했습니다." });
     } else {
       if (result.affectedRows > 0) {
         res.status(204).send(); // 삭제 성공 시 204 No Content 응답 반환
       } else {
-        res.status(404).json({ error: '해당 넘버를 가진 리소스를 찾을 수 없습니다.' });
+        res
+          .status(404)
+          .json({ error: "해당 넘버를 가진 리소스를 찾을 수 없습니다." });
       }
     }
   });
-})
-
-app.post('/write', async (req: Request, res: Response) => {
-  const {nickName, title, content, gameName, genre, detailGenre, url, personnel, deadLine} = req.body;
-  const insertQuery = `INSERT INTO tb_posts (writer, title, content, gameName, genre, detailGenre, url, personnel, deadLine) VALUES ('${nickName}', '${title}', '${content}', '${gameName}', '${genre}', '${detailGenre}', '${url}', '${personnel}', '${deadLine}')`
-  connection.query(insertQuery, (err, insertResult)=>{
-    if (err){
-      res.status(500).json({error : '글 작성중 오류가 발생했습니다.'})
-    } else {
-      res.json({status : 'success'});
-    }
-  })
 });
 
-app.put('/modify/:num', (req:Request, res:Response)=>{
+app.post("/write", async (req: Request, res: Response) => {
+  const {
+    nickName,
+    title,
+    content,
+    gameName,
+    genre,
+    detailGenre,
+    url,
+    personnel,
+    deadLine,
+  } = req.body;
+  const insertQuery = `INSERT INTO tb_posts (writer, title, content, gameName, genre, detailGenre, url, personnel, deadLine) VALUES ('${nickName}', '${title}', '${content}', '${gameName}', '${genre}', '${detailGenre}', '${url}', '${personnel}', '${deadLine}')`;
+  connection.query(insertQuery, (err, insertResult) => {
+    if (err) {
+      res.status(500).json({ error: "글 작성중 오류가 발생했습니다." });
+    } else {
+      res.json({ status: "success" });
+    }
+  });
+});
+
+app.put("/modify/:num", (req: Request, res: Response) => {
   const num = req.params.num;
-  const { title, content, gameName, genre, detailGenre, url, personnel, deadLine, status} = req.body;
- 
+  const {
+    title,
+    content,
+    gameName,
+    genre,
+    detailGenre,
+    url,
+    personnel,
+    deadLine,
+    status,
+  } = req.body;
+
   const updateQuerry = `
   UPDATE tb_posts
     SET title = ?, content = ?, gameName = ?, genre = ?, detailGenre = ?, url = ?,personnel = ?, deadLine = ?, status = ?
   WHERE 
   num = ?`;
 
-  connection.query(updateQuerry, [ title, content, gameName, genre, detailGenre, url, personnel, deadLine, status, num], (error, result)=>{
-    if (error) {
-      res.status(500).json({ message: '데이터베이스 업데이트 오류' });
-    } else {
-      res.status(200).json({ message: '데이터 업데이트 완료' });
+  connection.query(
+    updateQuerry,
+    [
+      title,
+      content,
+      gameName,
+      genre,
+      detailGenre,
+      url,
+      personnel,
+      deadLine,
+      status,
+      num,
+    ],
+    (error, result) => {
+      if (error) {
+        res.status(500).json({ message: "데이터베이스 업데이트 오류" });
+      } else {
+        res.status(200).json({ message: "데이터 업데이트 완료" });
+      }
     }
-  })
-})
+  );
+});
 
 // 서버 시작
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-  
 });
-
-
-
